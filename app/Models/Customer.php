4af77e1d544bde7dc1c1182c41247a28b1c03108
OmidKiani <o.kiani@sarmayex.com>
2024-05-22 13:40:26 +0430

    created model and migration of customer

---
+++ app/Models/Customer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'date_of_birth',
+        'phone_number',
+        'email',
+        'bank_account_number'
+    ];
+}
